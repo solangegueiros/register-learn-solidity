2a69b377f70840c2db9448008452d17198195eb4
Solange Gueiros <solangegueiros@gmail.com>
2023-05-08 23:00:17 +0200

    review count

--- register-13.sol
+++ register-13.sol
@@ -4,41 +4,27 @@ pragma solidity 0.8.19;
 // countChanges per address
 
 contract Register13 {
-    address public owner;
-    mapping (address => bool) public whiteList;
     mapping (address => string[]) public infos;
     mapping (address => uint) public countChanges;
 
     constructor() {
-        owner = msg.sender;
-        whiteList[msg.sender] = true;
         infos[msg.sender].push("Sol");
     }
 
     event InfoChange(address person, string oldInfo, string newInfo);
-    
-    modifier onlyOwner {
-        require(msg.sender == owner,"Only owner");
-        _;
-    }
-
-    modifier onlyWhitelist {
-        require(whiteList[msg.sender] == true, "Only whitelist");
-        _;
-    }
 
     function getInfo(uint index) public view returns (string memory) {
         return infos[msg.sender][index];
     }
-
-    function setInfo(uint index, string memory _info) public onlyWhitelist {
-        emit InfoChange (msg.sender, infos[msg.sender][index], _info);
-        infos[msg.sender][index] = _info;
-    }
     
-    function addInfo(string memory _info) public onlyWhitelist returns (uint index) {
+    function addInfo(string memory _info) public returns (uint index) {
         infos[msg.sender].push(_info);
         index = infos[msg.sender].length -1;
+    }
+
+    function setInfo(uint index, string memory _info) public {
+        emit InfoChange (msg.sender, infos[msg.sender][index], _info);
+        infos[msg.sender][index] = _info;
     }
 
     function listYourInfo() public view returns (string[] memory) {
@@ -48,12 +34,5 @@ contract Register13 {
     function listInfo(address account) public view returns (string[] memory) {
         return infos[account];
     }
-
-    function addMember (address _member) public onlyOwner {
-        whiteList[_member] = true;
-    }
-    
-    function delMember (address _member) public onlyOwner {
-        whiteList[_member] = false;
-    }    
+   
 }
